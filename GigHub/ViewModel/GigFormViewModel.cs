4a8f7a95a21b92bbaedddfe632a3006a953662b2
tommy.crafton <tommy.crafton@ruralsourcing.com>
2016-09-26 12:35:26 -0500

    added validation

--- GigHub/ViewModel/GigFormViewModel.cs
+++ GigHub/ViewModel/GigFormViewModel.cs
@@ -1,18 +1,32 @@
 ﻿using GigHub.Models;
+using GigHub.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.ViewModel
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
-        public DateTime DateTime {
-            get { return DateTime.Parse(string.Format("{0} {1}", Date, Time)); }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time)); 
         }
     }
 }
